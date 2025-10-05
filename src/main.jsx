9b2062449968a29160af880f4aabdca7fa8ae4f9
Abdelfattah <your.email@example.com>
2025-10-05 07:05:24 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import React from "react";
 import "./index.css";
 import App from "./App.jsx";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import { HeroUIProvider } from "@heroui/react";
 import AuthContextProvider from "./contexts/authContext.jsx";
 import UserDataContextProvider from "./contexts/userDataContext";
